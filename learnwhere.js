0519dc780d418211b5a937b7146b4219058856db
Jinpen <kx180415@gmail.com>
2026-05-25 22:45:57 +0800

    修复 learnwhere.js 中的变量引用，确保正确访问 body.data_body 属性

--- learnwhere.js
+++ learnwhere.js
@@ -1,20 +1,20 @@
 let body = JSON.parse($response.body);
 let url = $request.url;
 
-data_body.collins_vip.type = "vip";
-data_body.collins_vip.expire_timestamp = 11461492800000;
-data_body.collins_vip.expire_date = "2333-03-15";
+body.data_body.collins_vip.type = "vip";
+body.data_body.collins_vip.expire_timestamp = 11461492800000;
+body.data_body.collins_vip.expire_date = "2333-03-15";
 
-data_body.wordroot_vip.type = "vip";
-data_body.wordroot_vip.expire_timestamp = 11461492800000;
-data_body.wordroot_vip.expire_date = "2333-03-15";
+body.data_body.wordroot_vip.type = "vip";
+body.data_body.wordroot_vip.expire_timestamp = 11461492800000;
+body.data_body.wordroot_vip.expire_date = "2333-03-15";
 
-data_body.yanzhibao_vip.type = "vip"; 
-data_body.yanzhibao_vip.expire_timestamp = 11461492800000;
-data_body.yanzhibao_vip.expire_date = "2333-03-15";
+body.data_body.yanzhibao_vip.type = "vip"; 
+body.data_body.yanzhibao_vip.expire_timestamp = 11461492800000;
+body.data_body.yanzhibao_vip.expire_date = "2333-03-15";
 
-data_body.wordderive_vip.type = "vip";
-data_body.wordderive_vip.expire_timestamp = 11461492800000;
-data_body.wordderive_vip.expire_date = "2333-03-15";
+body.data_body.wordderive_vip.type = "vip";
+body.data_body.wordderive_vip.expire_timestamp = 11461492800000;
+body.data_body.wordderive_vip.expire_date = "2333-03-15";
 
 $done({ body: JSON.stringify(body) });
