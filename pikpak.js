336c297de7acda39d40ed41b95ba66b1fcd0cfb2
Jinpen <kx180415@gmail.com>
2026-05-25 18:43:19 +0800

    根据请求 URL 条件动态设置用户头像和过期时间

--- pikpak.js
+++ pikpak.js
@@ -1,7 +1,11 @@
 let body = JSON.parse($response.body);
+let url = $request.url;
 
-body.picture = "https://api.dicebear.com/9.x/open-peeps/svg?seed=Sara";
-body.data.expire = "2333-03-15T15:00:00+08:00";
-body.data.vipItem.expire = "2333-03-15T15:00:00+08:00";
+if (/\/user\/me/.test(url)) {
+  body.picture = "https://api.dicebear.com/9.x/open-peeps/svg?seed=Sara";
+} else if (/\/vip\/info/.test(url)) {
+  body.data.expire = "2333-03-15T15:00:00+08:00";
+  body.data.vipItem.expire = "2333-03-15T15:00:00+08:00";
+}
 
 $done({ body: JSON.stringify(body) });
