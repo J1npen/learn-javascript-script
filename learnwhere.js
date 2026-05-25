25f7b0a9f853c6c63d8a07e40b7b700ab2c9c521
Jinpen <kx180415@gmail.com>
2026-05-25 22:52:38 +0800

    更新 learnwhere.js 中 VIP 类型的过期时间和日期

--- learnwhere.js
+++ learnwhere.js
@@ -2,19 +2,19 @@ let body = JSON.parse($response.body);
 let url = $request.url;
 
 body.data_body.collins_vip.type = "vip";
-body.data_body.collins_vip.expire_timestamp = 11461492800000;
-body.data_body.collins_vip.expire_date = "2333-03-15";
+body.data_body.collins_vip.expire_timestamp = 1781015018000;
+body.data_body.collins_vip.expire_date = "2026-06-09";
 
 body.data_body.wordroot_vip.type = "vip";
-body.data_body.wordroot_vip.expire_timestamp = 11461492800000;
-body.data_body.wordroot_vip.expire_date = "2333-03-15";
+body.data_body.wordroot_vip.expire_timestamp = 1781015018000;
+body.data_body.wordroot_vip.expire_date = "2026-06-09";
 
 body.data_body.yanzhibao_vip.type = "vip"; 
-body.data_body.yanzhibao_vip.expire_timestamp = 11461492800000;
-body.data_body.yanzhibao_vip.expire_date = "2333-03-15";
+body.data_body.yanzhibao_vip.expire_timestamp = 1781015018000;
+body.data_body.yanzhibao_vip.expire_date = "2026-06-09";
 
 body.data_body.wordderive_vip.type = "vip";
-body.data_body.wordderive_vip.expire_timestamp = 11461492800000;
-body.data_body.wordderive_vip.expire_date = "2333-03-15";
+body.data_body.wordderive_vip.expire_timestamp = 1781015018000;
+body.data_body.wordderive_vip.expire_date = "2026-06-09";
 
 $done({ body: JSON.stringify(body) });
