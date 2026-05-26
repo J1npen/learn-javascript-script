17cfd40532605c4352cf0a60b2f082b0b530e541
Jinpen <kx180415@gmail.com>
2026-05-26 09:13:33 +0800

    更新 pikpak.js 中用户头像 URL 为新的图片链接

--- pikpak.js
+++ pikpak.js
@@ -2,7 +2,7 @@ let body = JSON.parse($response.body);
 let url = $request.url;
 
 if (/\/user\/me/.test(url)) {
-  body.picture = "https://api.dicebear.com/9.x/open-peeps/svg?seed=Sara";
+  body.picture = "https://i2.hdslb.com/bfs/face/3c339e642b9b772ff31ccc5fef566802c85852bc.jpg";
   body.phone_number = "俺不告诉你";
 
 } else if (/\/vip\/info/.test(url)) {
